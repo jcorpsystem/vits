2f8130d8e5d641baa555d4c2bd15378092fcb7ed
황원택 <wontaekhwang@gmail.com>
2023-06-14 13:01:12 +0900

    fix errors

--- text/cleaners.py
+++ text/cleaners.py
@@ -5,8 +5,8 @@ from text.korean import latin_to_hangul, number_to_hangul, divide_hangul, korean
 #from text.sanskrit import devanagari_to_ipa
 #from text.english import english_to_lazy_ipa, english_to_ipa2, english_to_lazy_ipa2
 #from text.thai import num_to_thai, latin_to_thai
-f#rom text.shanghainese import shanghainese_to_ipa
-f#rom text.cantonese import cantonese_to_ipa
+#from text.shanghainese import shanghainese_to_ipa
+#from text.cantonese import cantonese_to_ipa
 from text.ngu_dialect import ngu_dialect_to_ipa
 
 
